a37afa21511a52782920558a54aea443883c4ae3
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-12 17:56:55 -0300

    feat: Testando novo metodo da classe Verificador
    Testando metodo de verificador_codigo da classe Verificador

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,16 @@
-from regex import Regex
+from src.verificador import Verificador
+
 
 def get_inputs():
-    nome_produto = input(str("Digite o nome para o produto:"))
-    codigo = input(str("Digite o código do produto:"))
-    preco = input(str("Digite o preco do produto:"))
-    data = input(str("Digite a data de validade:"))
+    nome_produto = input("Digite o nome para o produto:")
+    codigo = input("Digite o código do produto:")
+    preco = input("Digite o preco do produto:")
+    data = input("Digite a data de validade:")
 
     return nome_produto, codigo, preco, data
 
-nome_produto, codigo, preco, data = get_inputs()
 
-print(Regex.verifica_nome_produto(nome_produto))
+if __name__ == "__main__":
+    nome_produto, codigo, preco, data = get_inputs()
+    print(Verificador.verificador_nome_produto(nome_produto))
+    print(Verificador.verificador_codigo(codigo))
