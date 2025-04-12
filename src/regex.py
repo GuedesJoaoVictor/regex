e8b0dc9040e1bee537e833c7b59e905ca48d7210
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-12 17:51:54 -0300

    style: Adicionando comentários
    Adicionando comentários importantes para formato das regex

--- src/regex.py
+++ src/regex.py
@@ -6,7 +6,7 @@ class Regex:
     Responsável por armazenar as expressões regulares utilizadas no sistema.
     """
     nome_produto = re.compile(
-        r"^(?P<nome>([A-Z][a-zA-Z0-9]+)(\s+[a-zA-Z0-9]+){2,})$"
+        r"^(?P<nome>([A-Z] ?[a-zA-Z0-9]+)(\s+[a-zA-Z0-9]+){2,} *)$"
     )
 
     codigo = re.compile(r"(?x)^(?P<letras>[A-Z]{6})"
@@ -20,8 +20,8 @@ class Regex:
         r"(?P<meses>(0[1-9])|1[0-2])/"
         r"(?P<anos>(20([0-1][0-9]|2[0-5]))|(19[5-9][0-9]))$")
 
-
     # Metodo estatico para verificar se o nome do produto é valido
+    # Formato Camiseta confortavel G
     @staticmethod
     def verifica_nome_produto(nome_produto):
         if Regex.nome_produto.search(nome_produto):
@@ -29,6 +29,7 @@ class Regex:
         return False
 
     # Metodo estatico para verificar se o codigo do produto é valido
+    # Formato ABCDEF1234
     @staticmethod
     def verifica_codigo(codigo):
         if Regex.codigo.search(codigo):
@@ -36,6 +37,7 @@ class Regex:
         return False
 
     # Metodo estatico para verificar se o preco do produto é valido
+    # Formatos: 1.000,00 | 100,000.00 | 99,99 | 10.99
     @staticmethod
     def verifica_preco(preco):
         if Regex.preco.search(preco):
